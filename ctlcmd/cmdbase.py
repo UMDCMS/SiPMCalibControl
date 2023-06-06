4b400d4edd7eb2ccb42f7d8d57a8f326e9082428
grace-blip <77363709+grace-blip@users.noreply.github.com>
2023-06-06 15:44:01 -0400

    Update cmdbase.py

--- ctlcmd/cmdbase.py
+++ ctlcmd/cmdbase.py
@@ -875,8 +875,8 @@ class rootfilecmd(controlcmd):
     self.array1.append(var1)
     self.array2.append(var2)
     
-    self.standardarr.append(time,det_id,self.gcoder.opx,self.gcoder.opy,self.gcoder.opz,
-                            self.gpio.adc_read(2),self.gpio.ntc_read(0),self.gpio.rtd_read(1))
+    self.standardarr.append([time,det_id,self.gcoder.opx,self.gcoder.opy,self.gcoder.opz,
+                            self.gpio.adc_read(2),self.gpio.ntc_read(0),self.gpio.rtd_read(1)])
     
     
     if self.n%10 ==0:
