062aabe7e2f8c199210d4ea86ecc7c4e1c932d74
grace-blip <77363709+grace-blip@users.noreply.github.com>
2023-06-06 16:02:29 -0400

    Update cmdbase.py

--- ctlcmd/cmdbase.py
+++ ctlcmd/cmdbase.py
@@ -881,7 +881,7 @@ class rootfilecmd(controlcmd):
     if self.n%10 ==0:
       for s in self.standardarr:
         print(*s)
-      rotated = [list(x) for x in zip(*self.standardarr)]
+      rotated = np.array(self.standardarr).T.tolist()
       for s in rotated:
         print(*s)
       
