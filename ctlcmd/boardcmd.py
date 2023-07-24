fde77e40ef5c4529e76e052e41a33c31b96763f0
vrundal-shah <vrundalshah05@gmail.com>
2023-07-24 17:53:26 -0400

    add boardcmd.py with board load and save commands

---
+++ ctlcmd/boardcmd.py
@@ -0,0 +1,71 @@
+"""
+  boardcmd.py
+
+  Commands that are used for modifying the board and its conditions. 
+  This includes loading and saving the coordinates calibration results.
+"""
+
+import ctlcmd.cmdbase as cmdbase
+import cmod.fmt as fmt
+import argparse
+import re
+import os
+import sys
+import time
+
+class save_board(cmdbase.controlcmd):
+  """
+  @brief Saving current board session.
+  """
+  def __init__(self, cmd):
+    cmdbase.controlcmd.__init__(self, cmd)
+
+  def add_args(self):
+    self.parser.add_argument('--filename',
+                             '-f',
+                             type=argparse.FileType(mode='r'),
+                             help="""
+                             The board configuration json filename to save current board session.""")
+
+  def run(self, args):
+    """
+    For the sake of clarity, device settings is split into each of their
+    functions. Notice that all function should have exception guards so the
+    subsequent settings can still be set if settings for one particular device
+    is bad or not available.
+    """
+    # add the board conditions
+    if args.filename:
+      try:
+        self.board.save_board(args.filename.name)
+      except RuntimeError as err:
+        self.printerr(str(err))
+        self.printwarn('Board saving has failed, skipping...')
+
+class load_board(cmdbase.controlcmd):
+  """
+  @brief Loading board from given config filename.
+  """
+  def __init__(self, cmd):
+    cmdbase.controlcmd.__init__(self, cmd)
+
+  def add_args(self):
+    self.parser.add_argument('--filename',
+                             '-f',
+                             type=argparse.FileType(mode='r'),
+                             help="""
+                             The board configuration json filename to load current board session.""")
+
+  def run(self, args):
+    """
+    For the sake of clarity, device settings is split into each of their
+    functions. Notice that all function should have exception guards so the
+    subsequent settings can still be set if settings for one particular device
+    is bad or not available.
+    """
+    # add the board conditions
+    if args.filename:
+      if self.load_board(args.filename):
+        self.printinfo(f"Board loaded from {args.filename.name}")
+      else:
+        self.printerr(f"Board loading from {args.filename.name} failed")
