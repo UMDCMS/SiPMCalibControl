59e8d2b265c1bbeb5428dbd0d38d91bbbebcdaf0
vrundal-shah <vrundalshah05@gmail.com>
2023-08-22 23:07:57 -0400

    remove javascript app and setup typescript react app

---
+++ gui/gui_client/src/Sections/TelemetryData.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const TelemetryData = (props: Props) => {
+  return <div>TelemetryData</div>;
+};
+
+export default TelemetryData;
---
+++ gui/gui_client/src/Sections/Tileboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Tileboard = (props: Props) => {
+  return <div>Tileboard</div>;
+};
+
+export default Tileboard;
